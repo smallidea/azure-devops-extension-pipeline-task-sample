35b849bc0a491bb18c69d4ba9271035e9c6c24a4
jasonchen <jasonchen@leansoftx.com>
2021-05-13 10:58:25 +0800

    📦 异常时disable && 流水线中task图标

--- src/tasks/sample/index.ts
+++ src/tasks/sample/index.ts
@@ -1,15 +1,22 @@
 import * as tl from 'azure-pipelines-task-lib/task'
 
 async function run() {
-    console.log("start -----------------------------");
+    try{
 
-    let input = tl.getInput("sampleInput", true)!; // 获取流水线任务中录入框输入的值
-    if (input.toLowerCase() != 'jason') {
-        tl.setResult(tl.TaskResult.Failed, '和Jason打个招呼吧！*^-^*');
-        return;
+        console.log("start -----------------------------");
+
+        let input = tl.getInput("sampleInput", true)!; // 获取流水线任务中录入框输入的值
+        if (input.toLowerCase() != 'jason') {
+            tl.setResult(tl.TaskResult.Failed, '和Jason打个招呼吧！*^-^*');
+            return;
+        }
+    
+        console.log("end -----------------------------");
+
+    }catch(error){
+        tl.debug(JSON.stringify(error as Error, null, 4));
+        tl.setResult(tl.TaskResult.Failed, typeof error == 'string' ? error as string : error.meesage);
     }
-
-    console.log("end -----------------------------");
 }
 
 run();
