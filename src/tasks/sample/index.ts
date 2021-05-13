fa83c4d26fd4cadea931aaf9456ca47724d34e9d
jasonchen <jasonchen@leansoftx.com>
2021-05-13 10:16:08 +0800

    🚀

--- src/tasks/sample/index.ts
+++ src/tasks/sample/index.ts
@@ -3,7 +3,7 @@ import * as tl from 'azure-pipelines-task-lib/task'
 async function run() {
     console.log("start -----------------------------");
 
-    let input = tl.getInput("input", true); // 获取流水线任务中录入框输入的值
+    let input = tl.getInput("sampleInput", true)!; // 获取流水线任务中录入框输入的值
     if (input.toLowerCase() != 'jason') {
         tl.setResult(tl.TaskResult.Failed, '和Jason打个招呼吧！*^-^*');
         return;
